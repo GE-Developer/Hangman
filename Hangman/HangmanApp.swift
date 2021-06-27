d15acb1b370cd7bd77b5ee0bbbcef7b2b72660d0
Michael Bukhrashvili <on-line-misha@mail.ru>
2021-06-27 16:33:28 +0300

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Mikhail Bukhrashvili on 27.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
